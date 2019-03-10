330e319810eba3a0ee79d522c4ebcf04ebc3bc21
Federico Kauffman <federico@streaver.com>
2019-03-10 15:31:40 -0300

    Use tab caching service to remove participants when the call is finished (or the tab is closed)

--- src/background.js
+++ src/background.js
@@ -1,22 +1,12 @@
 import RegistrationService from './services/registration';
+import CallTabPersistanceService from './services/call-tab-persistance';
 import BackgroundPopupCommunicationService from './services/background-popup-communication';
 import ContentBackgroundCommunicationService from './services/content-background-communication';
 
 const GOOGLE_MEET_HOST = 'meet.google.com';
-const ACTIVE_CALL_TABS = [];
-const ACTIVE_CALL_TABS_MAPPING = {};
-const USER_DATA_CACHE = {};
 
 function isCallTab(status, host, serviceHost) {
   return host === serviceHost && status === 'complete';
-}
-
-async function getUserData(tabId, options = { useCache: false }) {
-  if (options.useCache) {
-    return USER_DATA_CACHE[tabId];
-  }
-
-  return ContentBackgroundCommunicationService.sendAction(tabId, ContentBackgroundCommunicationService.GET_USER_DETAILS_ACTION_NAME);
 }
 
 chrome.tabs.onUpdated.addListener(async (tabId, _, tab) => {
@@ -24,36 +14,31 @@ chrome.tabs.onUpdated.addListener(async (tabId, _, tab) => {
 
   if (isCallTab(tab.status, url.host, GOOGLE_MEET_HOST)) {
     const meetingId = url.pathname.replace('/', '');
+    const details = await ContentBackgroundCommunicationService.sendAction(tabId, ContentBackgroundCommunicationService.GET_USER_DETAILS_ACTION_NAME);
 
     BackgroundPopupCommunicationService.setMeetingId(meetingId);
 
-    if (!ACTIVE_CALL_TABS.includes(tabId)) {
-      ACTIVE_CALL_TABS.push(tabId);
-      ACTIVE_CALL_TABS_MAPPING[tabId] = tab;
-    }
-
-    const details = await getUserData(tabId);
-
     if (details) {
-      USER_DATA_CACHE[tabId] = details;
-
+      CallTabPersistanceService.save(tab, meetingId, details);
       await RegistrationService.registerUser(tab.url, details);
+    } else {
+      const tabData = CallTabPersistanceService.find(tab.id);
+
+      if (tabData) {
+        BackgroundPopupCommunicationService.removeMeetingId();
+        RegistrationService.unregisterUser(tabData.tabUrl, tabData.userDetails);
+        CallTabPersistanceService.remove(tabData.tabId);
+      }
     }
   }
 });
 
 chrome.tabs.onRemoved.addListener(async tabId => {
-  const tabPosition = ACTIVE_CALL_TABS.indexOf(tabId);
+  const tab = CallTabPersistanceService.find(tabId);
 
-  if (tabPosition !== -1) {
-    const tab = ACTIVE_CALL_TABS_MAPPING[tabId];
-
+  if (tab) {
     BackgroundPopupCommunicationService.removeMeetingId();
-
-    const details = await getUserData(tabId, { useCache: true });
-
-    RegistrationService.unregisterUser(tab.url, details);
-
-    ACTIVE_CALL_TABS.splice(tabPosition, 1);
+    RegistrationService.unregisterUser(tab.tabUrl, tab.userDetails);
+    CallTabPersistanceService.remove(tabId);
   }
 });
